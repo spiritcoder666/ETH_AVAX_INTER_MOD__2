cdc0e2c0ffe6ce2260fc756f7dd7ff1c7ef6424d
spiritcoder666 <144348608+spiritcoder666@users.noreply.github.com>
2024-06-25 09:40:36 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,13 @@
 
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    localhost: {
+      url: "http://localhost:8545", // Adjust port number if needed
+    },
+  },
+};
+
